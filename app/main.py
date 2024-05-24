f457699999edeaaa5207e5a31995d589a6b8d021
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-05-24 20:52:25 +0100

    Refactor shell built-in commands handling

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
 import sys
 
 
+def handle_exit(args):
+    sys.exit(int(args[0]) if args else 0)
+
+
+def handle_echo(args):
+    print(" ".join(args))
+
+
+def handle_type(args):
+    if args[0] in builtins:
+        print(f"{args[0]} is a shell builtin")
+    else:
+        print(f"{args[0]} not found")
+
+
+builtins = {
+    "exit": handle_exit,
+    "echo": handle_echo,
+    "type": handle_type,
+}
+
+
 def main():
     while True:
         sys.stdout.write("$ ")
@@ -8,11 +30,9 @@ def main():
 
         command, *args = input().split(" ")
 
-        if command == "exit":
-            exit_code = int(args[0]) if args else 0
-            sys.exit(exit_code)
-        elif command == "echo":
-            print(" ".join(args))
+        if command in builtins:
+            builtins[command](args)
+            continue
         else:
             print(f"{command}: command not found")
 
