efa6eca27e2aede7a20752eeaf27a5eedfcbb232
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-05-24 20:45:18 +0100

    Refactor main.py: Handle 'exit' command and improve error message.

--- app/main.py
+++ app/main.py
@@ -6,9 +6,14 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        command = input()
-        sys.stdout.write(f"{command}: command not found\n")
-        sys.stdout.flush()
+        command, *args = input().split(" ")
+
+        if command == "exit":
+            exit_code = int(args[0]) if args else 0
+            sys.exit(exit_code)
+        else:
+            sys.stdout.write(f"{command}: command not found\n")
+            sys.stdout.flush()
 
 
 if __name__ == "__main__":
